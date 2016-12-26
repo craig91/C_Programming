4c3b95a48b1849db564b59206e96b91817febe38
craig91 <craigdejean1@gmail.com>
2016-12-26 15:36:22 -0500

    Double quotes in string

---
+++ blorf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello \nMy name is \"Blorf\""); // <-- In order to use "" in your string you have to surround the word you want with 
+                                            // double quotes with \. 
+
+    return(0);
+}
