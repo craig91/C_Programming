fc1148d8c28ee4a80be73541088debeab4724eeb
Craig Dejean <thelegendarycraig@yahoo.com>
2017-10-28 12:39:31 -0400

    Temperature program using symbolic constants

--- Fahrenheiht-Centigrade/printFahrenheihtForLoop.c
+++ Fahrenheiht-Centigrade/printFahrenheihtForLoop.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+#define LOWER 0 // Symbolic constants -> variables that cannot be changed 
+                // each occurence of LOWER will equal 0.
+#define UPPER 300
+#define STEP 20
+
+
 int main() {
     int fahr;
     
-       for(fahr = 0; fahr <= 300; fahr = fahr + 20) 
+       for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP) 
     // for(fahr = 300; fahr >= 0; fahr = fahr - 20) --> Challenge print in reverse order.
         printf("%4d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
 }
