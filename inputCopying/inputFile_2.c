eb41cf1caaefb7d0955f70a934280eb31c835fda
Craig Dejean <thelegendarycraig@yahoo.com>
2017-10-28 13:57:27 -0400

    input file version 2

---
+++ inputCopying/inputFile_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+
+    while((c = getchar()) != EOF)
+        putchar(c);
+}
+
+// In C, any assignment can be used as an expression.
+// c = getchar() while part of a while loop is an example of this.
+
+
+// The program gets a character.
+// The character is assigned to c --> c = getchar();
+// Then it is tested to see if the character is the EOF(signal) --> while((c = getchar()) != EOF)
+// If it isn't, the body of the whilel loop is executed, printing the character.
+// Then the while repeats until main() is terminated.
+
+
+
