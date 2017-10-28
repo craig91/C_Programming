fada730b59f1a70c3ea79ffe7e5b5c264da72631
Craig Dejean <thelegendarycraig@yahoo.com>
2017-10-28 13:33:29 -0400

    Added comments

--- inputCopying/inputFile.c
+++ inputCopying/inputFile.c
@@ -2,9 +2,18 @@
 
 int main() {
     int c;
-    c = getchar();
+    c = getchar(); // contains the next value of input
+    
     while (c != EOF) {
-        putchar(c);
+        
+        putchar(c); // prints the contents of variable c to some output medium.
+        
         c = getchar();
     }
 }
+
+// getchar() --> A function in the standard library that fetches
+// the next input character ( input as in what you type into the terminal)
+// each time it is called.
+
+// EOF --> End of file signal.
