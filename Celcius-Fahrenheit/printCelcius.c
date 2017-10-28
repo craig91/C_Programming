b33e34b5181ea8993af55420538c81432eabe155
Craig Dejean <thelegendarycraig@yahoo.com>
2017-10-28 10:53:48 -0400

    Celcius to Fahrenheit source

--- Celcius-Fahrenheit/printCelcius.c
+++ Celcius-Fahrenheit/printCelcius.c
@@ -8,12 +8,12 @@ int main() {
     upper = 300;
     step = 20;
 
-    celcius = lower;
+    fahr = lower;
     printf("This is celcius to fahrenheit!\n");
-    while(celcius <= upper) {
-        celcius = (5.0/9.0) + (fahr - 32.0);
+    while(fahr <= upper) {
+        celcius = (5.0/9.0) * (fahr - 32.0);
 
-        printf("%8.1f Of %5.0f\n ", celcius, fahr);
+        printf("%8.1f Of %5.0f\n ", fahr, celcius);
 
         fahr = fahr + step;
     }
