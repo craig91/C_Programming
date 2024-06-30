82110024ae359253d4958a900720323214b08121
craig91 <craigdejean1@gmail.com>
2024-06-30 10:53:51 -0400

    small changes

--- c_begin/display_vars/vars.c
+++ c_begin/display_vars/vars.c
@@ -4,11 +4,16 @@ int main() {
     int num = 100;
     double pi = 3.1415926536;
 
+    double testDecimal = 1.2345;
+
+    printf("Test Decimal --> %.2f\n", testDecimal);
+    printf("--------------- \n");
+
     printf("Integer is %d \n", num);
     printf("Values are %d and %f \n", num, pi);
     
 
-    printf("%%7d displays %7d \n", num);
+    printf("%%7d displays %3d \n", num);
     printf("%%07d displays %07d\n", num);
     
     printf("Pi is approximately %1.10f\n", pi);
