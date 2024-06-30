d1981f7bf0a1b1fbacfe0fad0697bdb6740c229a
craig91 <craigdejean1@gmail.com>
2024-06-30 09:21:09 -0400

    var formatters

---
+++ c_begin/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main() {	
+    int num1, num2;
+    char letter;
+    float decimal = 7.5;
+    num1 = 100;
+    num2 = 200;
+    letter = 'A';
+
+    printf("hello world\n");
+    return 0;
+}
