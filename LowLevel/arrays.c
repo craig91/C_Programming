19f1f98b2298fd8d0e47b8bbc4927f9db03c3577
craig91 <craigdejean1@gmail.com>
2024-12-19 23:11:23 -0500

    added array file

--- LowLevel/arrays.c
+++ LowLevel/arrays.c
@@ -3,5 +3,11 @@
 #define MAX_IDS 32
 
 int main() {
-    int ids[MAX_IDS];
+    // int ids[MAX_IDS]; // create array of 32 elements of int size (4 bytes)
+    int ids[MAX_IDS] = {1,2,3};
+    printf("%d\n", ids[0]);
+
+    ids[3] = 0x41;
+    printf("%d\n", ids[3]);
+
 }
