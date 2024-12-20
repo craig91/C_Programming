bb7b01861ac3c27eb0928512095670296178c73b
craig91 <craigdejean1@gmail.com>
2024-12-20 16:08:11 -0500

    completed exercise 3

---
+++ LowLevel/Practice/minMax.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int minmax[] = {7,2,19,5,11,3};
+    int smallest = minmax[0];
+    int largest = minmax[0];
+    for (int i = 0; i < 6; i++) {
+        if(minmax[i] < smallest) {
+            smallest = minmax[i];
+        }
+        if(minmax[i] > largest) {
+            largest = minmax[i];
+        } 
+    }
+    printf("The maximum value in the array is: %i\n", largest);
+    printf("The smallese value in the array is: %i\n", smallest);
+    return 0;
+}
