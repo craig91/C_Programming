b545e9aba8f1f036c79a681ab9dceae1c977e46f
craig91 <craigdejean1@gmail.com>
2024-12-23 13:57:06 -0500

    .

---
+++ LowLevel/string2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MAX_IDS 32
+
+int main() {
+    int ids[MAX_IDS] = {0,1,2};
+    char mystr[] = {'h','e','l','l','o'};
+    char *myotherstr = "hello";
+
+    printf("%s\n", mystr);
+
+    // strcpy(dest, src);
+    // strncopy(dest, src, n);
+}
