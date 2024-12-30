92878aec06f3a56dcc1ae4d2cc28056f3b1d0e50
craig91 <craigdejean1@gmail.com>
2024-12-30 12:48:23 -0500

    Updates

---
+++ LowLevel/Practice/guessingGame.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+#define LIMIT 100
+
+int isValidNum(const char *input) {
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (!isdigit(input[i])) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+
+void guessing_game() {
+    char input[100];
+    srand(time(NULL));
+    int val = rand() % (LIMIT + 1);
+
+    while(1){
+        printf("Guess a number (0 to %d): ", LIMIT);
+        scanf("%s", input);
+
+        if(!isValidNum(input)) {
+            printf("This is not a valid number! Exiting...\n");
+            break;
+        }
+
+        int userNum = atoi(input);
+        
+        if(userNum == val) {
+                printf("You guessed right! The number was %d.\n", val);
+                break;
+            } else if (userNum > val) {
+                printf("Too High!\n");
+            } else {
+                printf("Too Low!\n");
+            }
+    }
+}
+    
+
+int main() {
+    guessing_game();
+	return 0;
+}
+
